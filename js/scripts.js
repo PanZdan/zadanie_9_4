b91974b0b835c8506bd5f37d3eabdd9fdb0d2840
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-12-02 18:25:32 +0100

    Changed scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-// scripts.js
+// // scripts.js
 
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-console.log(text.length);
-console.log(text);
 
-var dinosaur = 'triceratops'
-var dinosaurUpperCased = dinosaur.toUpperCase();
-var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-console.log(textCharsAfter);
-console.log(textCharsAfter.length / 2);
-console.log(textCharsAfter.slice(0, textCharsAfter.length / 2));
+function drawTree(n) {
+	for (var i = 0; i < n; i++) {
+		var line = '';
+		for (var j = 0; j < (n - i); j++) {
+			line += ' ';
+	}
+		var star = '';
+	for (var k = 0; k <= i * 2; k++) {
+			star += '*';
+	}
+	console.log(line, star);
+	} 
+}
+drawTree(10);
