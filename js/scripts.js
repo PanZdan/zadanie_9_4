b461d28f500d46ed0a38169c344fbe1d66e758f2
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-12-05 23:38:15 +0100

    Changed js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function drawTree(n) {
 	for (var i = 0; i < n; i++) {
 		var line = '';
 		for (var j = 0; j < (n - i); j++) {
-			line += 'O';
+			line += ' ';
 		}
 		var star = '';
 		for (var k = 0; k <= i * 2; k++) {
